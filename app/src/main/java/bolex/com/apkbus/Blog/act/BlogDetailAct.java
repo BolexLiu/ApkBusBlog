4881ce9633dded832d83ef29cb3d0f7812ce571f
bolexLiu <305853782@qq.com>
2017-08-04 20:35:12 +0800

    优化排版

--- app/src/main/java/bolex/com/apkbus/Blog/act/BlogDetailAct.java
+++ app/src/main/java/bolex/com/apkbus/Blog/act/BlogDetailAct.java
@@ -1,14 +1,15 @@
 package bolex.com.apkbus.Blog.act;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.widget.NestedScrollView;
 import android.transition.Slide;
 import android.view.View;
-import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -18,7 +19,6 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.elyeproj.loaderviewlibrary.LoaderTextView;
-import com.just.library.AgentWeb;
 
 import bolex.com.apkbus.R;
 import bolex.com.apkbus.View.GlideCircleTransform;
@@ -54,11 +54,9 @@ public class BlogDetailAct extends BusBaseActivity {
     CollapsingToolbarLayout appbar;
 
 
-    private AgentWeb mAgentWeb;
-
     final String BlogBaseUrl = "http://www.apkbus.com/";
     private String url;
-    private WebSettings webSettings;
+    private WebSettings mWebSettings;
     private String authorName;
     private String headUrl;
     private String title1;
@@ -88,7 +86,6 @@ public class BlogDetailAct extends BusBaseActivity {
     }
 
     private void initDate() {
-//        llWeb.removeView(nsView);
         llRoot.setBackgroundResource(color);
         Glide.with(mActivity).
                 load(headUrl)
@@ -98,36 +95,61 @@ public class BlogDetailAct extends BusBaseActivity {
         title.setText(title1);
         tvName.setText(authorName);
         llWeb.loadUrl(BlogBaseUrl + url);
-        webSettings = llWeb.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-//        webSettings.setTextZoom(fontSize);
+        settings(llWeb);
         llWeb.setWebViewClient(mWebViewClient);
-        webSettings.setLayoutAlgorithm(android.webkit.WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
-//        mAgentWeb = AgentWeb.with(this)//传入Activity
-//                .setAgentWebParent(llWeb, new ViewGroup.LayoutParams(-1, -1))
-//                .useDefaultIndicator()// 使用默认进度条
-//                .defaultProgressBarColor() // 使用默认进度条颜色
-//                .setWebViewClient(mWebViewClient)
-//                .setWebView((WebView) getLayoutInflater().inflate(R.layout.web_view, null))
-//                .createAgentWeb()//
-//                .ready()
-//                .go(BlogBaseUrl + url);
-//        webSettings = mAgentWeb.getAgentWebSettings().getWebSettings();
-//        webSettings.setTextZoom(fontSize);
-//        webSettings.setJavaScriptEnabled(true);
+        llWeb.setWebChromeClient(mWebChromeClient);
     }
 
-
-    private WebViewClient mWebViewClient = new WebViewClient() {
+    boolean isNeedExe = true;
+    private WebChromeClient mWebChromeClient = new WebChromeClient() {
         @Override
-        public void onPageFinished(WebView view, String url) {
+        public void onProgressChanged(WebView view, int newProgress) {
 
-            view.loadUrl("javascript:function myFunction(){var $jquery = jQuery.noConflict();var content=$jquery('.article');$jquery('body').empty();content.css({background:\"#fff\",position:\"absolute\",top:\"0\",left:\"0\"});$jquery('body').append(content);$jquery(\"div\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");$jquery(\"a\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");$jquery(\"h1\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");$jquery(\"h2\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");$jquery(\"h3\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");$jquery(\"h4\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");$jquery(\"img\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");}");
-            view.loadUrl("javascript:myFunction()");
-            super.onPageFinished(view, url);
-            pDialog.cancel();
+            if (newProgress > 95 && isNeedExe) {
+                isNeedExe = !isNeedExe;
+                view.loadUrl("javascript:function myFunction(){\n" +
+                        "var $jquery = jQuery.noConflict();\n" +
+                        "var content=$jquery('.article');\n" +
+                        "$jquery('body').empty();\n" +
+                        "content.css({\n" +
+                        "background:\"#fff\",\n" +
+                        "position:\"absolute\",\n" +
+                        "top:\"0\",left:\"0\",\n" +
+                        "});\n" +
+                        "$jquery('body').append(content);\n" +
+                        "$jquery(\"div\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"a\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"h1\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"h2\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"h3\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"h4\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"h5\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"h6\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"img\").removeAttr(\"class\").removeAttr(\"style\").removeAttr(\"id\");\n" +
+                        "$jquery(\"img\").css({width: \"100%\",height:\"100%\",objecFit:\"cover\"});\n" +
+                        "$jquery(\"h1\").css({paddingBottom: \"0.3em\",fontSize:\"2em\",borderBottom:\"1px solid #eaecef\"});\n" +
+                        "$jquery(\"h2\").css({paddingBottom: \"0.3em\",fontSize:\"1.5em\",borderBottom:\"1px solid #eaecef\"});\n" +
+                        "$jquery(\"h3\").css({fontSize:\"1.25em\"});\n" +
+                        "$jquery(\"h4\").css({fontSize:\"1em\"});\n" +
+                        "$jquery(\"h5\").css({fontSize:\"0.875em\"});\n" +
+                        "$jquery(\"h6\").css({fontSize:\"0.85em\"});\n}");
+                view.loadUrl("javascript:myFunction()");
+                pDialog.cancel();
+            }
+
+            super.onProgressChanged(view, newProgress);
         }
     };
+    private WebViewClient mWebViewClient = new WebViewClient() {
+        @Override
+        public void onPageStarted(final WebView view, String url, Bitmap favicon) {
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+        }
+    };
+
 
     private void intView() {
         pDialog = new SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE);
@@ -148,38 +170,18 @@ public class BlogDetailAct extends BusBaseActivity {
             case R.id.tv_sub:
                 if (fontSize > 100) {
                     fontSize -= 50;
-                    webSettings.setTextZoom(fontSize);
+                    mWebSettings.setTextZoom(fontSize);
                 }
                 break;
             case R.id.tv_add:
                 if (fontSize < 700) {
                     fontSize += 50;
-                    webSettings.setTextZoom(fontSize);
+                    mWebSettings.setTextZoom(fontSize);
                 }
                 break;
         }
     }
 
-    private void swichTextSize(int fontSize) {
-        switch (fontSize) {
-            case 1:
-                webSettings.setTextSize(WebSettings.TextSize.SMALLEST);
-
-                break;
-            case 2:
-                webSettings.setTextSize(WebSettings.TextSize.SMALLER);
-                break;
-            case 3:
-                webSettings.setTextSize(WebSettings.TextSize.NORMAL);
-                break;
-            case 4:
-                webSettings.setTextSize(WebSettings.TextSize.LARGER);
-                break;
-            case 5:
-                webSettings.setTextSize(WebSettings.TextSize.LARGEST);
-                break;
-        }
-    }
 
     private void setupWindowAnimations() {
         Slide slide = null;
@@ -192,6 +194,54 @@ public class BlogDetailAct extends BusBaseActivity {
 
     @OnClick(R.id.iv_back)
     public void onViewClicked() {
-        finish();
+        onBackPressed();
+    }
+
+
+    private void settings(WebView webView) {
+        mWebSettings = webView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setSupportZoom(true);
+        mWebSettings.setBuiltInZoomControls(false);
+        mWebSettings.setSavePassword(false);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            mWebSettings.setMixedContentMode(0);
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            webView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        } else if (Build.VERSION.SDK_INT < 19) {
+            webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+        }
+
+//        mWebSettings.setRenderPriority(android.webkit.AgentWebSettings.RenderPriority.HIGH);
+        mWebSettings.setTextZoom(fontSize);
+        mWebSettings.setDatabaseEnabled(true);
+        mWebSettings.setAppCacheEnabled(true);
+        mWebSettings.setLoadsImagesAutomatically(true);
+        mWebSettings.setSupportMultipleWindows(false);
+        mWebSettings.setBlockNetworkImage(false);//是否阻塞加载网络图片  协议http or https
+        mWebSettings.setAllowFileAccess(true); //允许加载本地文件html  file协议, 这可能会造成不安全 , 建议重写关闭
+        mWebSettings.setAllowFileAccessFromFileURLs(false); //通过 file url 加载的 Javascript 读取其他的本地文件 .建议关闭
+        mWebSettings.setAllowUniversalAccessFromFileURLs(false);//允许通过 file url 加载的 Javascript 可以访问其他的源，包括其他的文件和 http，https 等其他的源
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        if (Build.VERSION.SDK_INT >= 19)
+            mWebSettings.setLayoutAlgorithm(android.webkit.WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        else
+            mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NORMAL);
+        mWebSettings.setLoadWithOverviewMode(true);
+        mWebSettings.setUseWideViewPort(true);
+        mWebSettings.setDomStorageEnabled(true);
+        mWebSettings.setNeedInitialFocus(true);
+        mWebSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+        mWebSettings.setDefaultFontSize(22);
+        mWebSettings.setMinimumFontSize(16);//设置 WebView 支持的最小字体大小，默认为 8
+        mWebSettings.setGeolocationEnabled(true);
+        //适配5.0不允许http和https混合使用情况
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mWebSettings.setMixedContentMode(android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        //缓存文件最大值
+        mWebSettings.setAppCacheMaxSize(Long.MAX_VALUE);
     }
 }
